1a06807b1748ef398901668356cee8a2c209c7d9
Leonen Hyuven <lhyuvengyem@gmail.com>
2022-04-07 14:59:34 +0800

    Added axios, POST/create and GET/table working

--- frontend/src/components/AppForm.vue
+++ frontend/src/components/AppForm.vue
@@ -1,18 +1,50 @@
 <script>
 import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
+import Env from "../assets/env.js";
+
 export default {
   name: 'AppForm',
+  data(){
+    return {
+      weight : {
+        name: '',
+        current_weight: '',
+        log: ''
+      }
+    }
+
+  
+  },
+  methods : {
+    submitForm(){
+      const apiURL = Env.baseURL;
+      axios.post(apiURL,this.weight)
+      .then(()=>{
+        this.$router.push('/table');
+        this.weight = {
+          name: '',
+          current_weight: '',
+          log: ''
+        }
+        console.log("Success")
+      })
+      .catch((err)=>{
+        console.log(err)
+      })
+    }
+  }
 
 }
 </script>
 <template>
-<form>
+<form @submit.prevent = "submitForm">
 <label class = 'form-label'> Name </label>
-<input id = 'weight' class = 'form-control' type = 'text' required placeholder = "Enter name..">
+<input  v-model = "weight.name" id = 'weight' class = 'form-control' type = 'text' required placeholder = "Enter name..">
 <label class = 'form-label'> Weight </label>
-<input id = 'weight' class = 'form-control' type = 'number' step = '0.01' required placeholder = "Enter weight..">
+<input v-model = "weight.current_weight" id = 'weight' class = 'form-control' type = 'number' step = '0.01' required placeholder = "Enter weight..">
 <label class = 'form-label'> Logs</label>
-<textarea id = 'logs' rows = '3'  class = 'form-control' placeholder = "Enter activities, foods, or any details you want to share..">
+<textarea v-model = "weight.log" id = 'logs' rows = '3'  class = 'form-control' placeholder = "Enter activities, foods, or any details you want to share..">
 </textarea>
 <button class = 'btn btn-success float-right' id = 'submit_btn'> Submit </button>
 </form>
--- frontend/src/components/AppTable.vue
+++ frontend/src/components/AppTable.vue
@@ -11,18 +11,43 @@ Date
 <th>
 Weight
 </th>
+<th>
+Log
+</th>
 </tr>
 </thead>
 <tbody>
+<tr v-for = "weight  in Weights.data" :key="weight._id">
+<td>{{weight.name}}</td>
+<td>{{weight.createdAt}}</td>
+<td>{{weight.current_weight}}</td>
+<td>{{weight.log}}</td>
+</tr>
 </tbody>
 </table>
 </template>
 
 <script>
 import "bootstrap/dist/css/bootstrap.min.css";
+import Env from "../assets/env.js";
+import axios from "axios";
 export default {
   name: 'AppTable',
-
+  data(){
+    return {
+      Weights: []
+    }
+  },
+  created(){
+    let apiURL = Env.baseURL;
+    axios.get(apiURL)
+    .then((res)=>{
+      this.Weights = res.data // could be res, but the api returns this format: {error, data, message}
+    })
+    .catch((err)=>{
+      console.log(err)
+    })
+  }
 }
 </script>
 
