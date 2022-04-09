da12a59019fda7d4932a6b2a64e394a9c085f72f
Leonen Hyuven <lhyuvengyem@gmail.com>
2022-04-09 15:10:53 +0800

    Added compare target to current weight in chart component

--- frontend/src/components/AppGraph.vue
+++ frontend/src/components/AppGraph.vue
@@ -1,5 +1,10 @@
 <template>
   <div>
+    <form @submit.prevent = "compareWeight">
+    <label class = 'form-label'> Compare </label>
+    <input id = 'target_weight' class = 'form-control' type = 'number' required placeholder = "Enter target weight in kg..">
+    <button class = 'btn btn-success float-right' id = 'check_btn' > Check</button>
+    </form>
     <canvas id="this_chart"></canvas>
   </div>
 </template>
@@ -13,6 +18,7 @@ import moment from 'moment';
 let apiURL = Env.baseURL;
 let labels = new Array();
 let datasets = new Array();
+let target_datasets = new Array();
 axios.get(apiURL)
 .then((res)=>{
 
@@ -20,6 +26,7 @@ axios.get(apiURL)
     res.data.data.forEach((val)=>{
     labels.push(moment(val.createdAt).format('MMMM Do YYYY'));
     datasets.push(val.current_weight);
+    target_datasets.push(0);
     console.log(val);
     })
 
@@ -39,15 +46,15 @@ const graphData = {
           borderColor: "#36495d",
           borderWidth: 3
         },
-        /*
+    
         {
-          label: "",
-          data: [0.166, 2.081, 3.003, 0.323, 954.792, 285.886, 43.662, 51.514],
+          label: "Target Weight",
+          data: target_datasets,
           backgroundColor: "rgba(71, 183,132,.5)",
           borderColor: "#47b784",
           borderWidth: 3
         }
-        */
+        
       ]
     },
     options: {
@@ -80,6 +87,17 @@ export default {
     const ctx = document.getElementById('this_chart');
     new Chart(ctx, this.graphData);
   },
+  methods:{
+    compareWeight(){
+
+        target_datasets.forEach((val,i)=>{
+          target_datasets[i] = document.getElementById('target_weight').value;
+        })
+        const ctx = document.getElementById('this_chart');
+        new Chart(ctx, this.graphData);
+      
+    }
+  }
 
 }
 </script>
